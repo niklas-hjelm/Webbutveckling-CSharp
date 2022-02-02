ab8e20638d6cf2f9803f337ffbbfd619e3f81185
NiklasHj <54146045+NiklasHj@users.noreply.github.com>
2022-02-02 13:46:49 +0100

    Update 2 2/2

--- assets/basicJavaScript/js/script.js
+++ assets/basicJavaScript/js/script.js
@@ -1,19 +1,31 @@
+let one = 6,
+  two = 6,
+  counterVariable = 0;
+
+const result = one / two;
+
 function addNumbers(numOne, numTwo) {
   return numOne + numTwo;
 }
 
-function wohoo() {
-  one = 6;
-  document.getElementById("testList").innerHTML = "";
+function showList() {
   while (one > 0) {
     document.getElementById("testList").innerHTML += `<li>${--one}</li>`;
   }
 }
 
-var one = 6;
-var two = 6;
+function clearList() {
+  one = 6;
+  document.getElementById("testList").innerHTML = "";
+}
 
-const result = one / two;
+function showAlert() {
+  alert("Detta är en alert-box!");
+}
+
+function counter() {
+  counterVariable++;
+}
 
 if (one === two) {
   let res2 = one + two;
@@ -29,10 +41,19 @@ if (one === two) {
 
 document.getElementById("summa").innerHTML += ` ${addNumbers(6, 2)}`;
 
-document.getElementById("namn").innerHTML = "Niklas";
+document.getElementById("namn").innerHTML = "<p>Apa</p>";
 document.getElementById("namn2").innerHTML = "Niklas2";
 
-document.getElementById("leaves").src = "images/picTwo.jpeg";
+// document.getElementById("leaves").src = "images/picTwo.jpeg";
 document.getElementById("namn2").style.color = "red";
 document.getElementById("namn").style.color = "black";
-document.getElementById("leaves").style.borderRadius = "50%";
+// document.getElementById("leaves").style.borderRadius = "50%";
+
+function varTest() {
+  var test = 0;
+  while (test < 5) {
+    test++;
+    let a = 0;
+  }
+  let a = 0;
+}
