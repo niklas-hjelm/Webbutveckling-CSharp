e7d97af06d8f5f27e132444963ce20ee76bc8679
NiklasHj <54146045+NiklasHj@users.noreply.github.com>
2022-02-10 10:08:04 +0100

    json demo och fetch

---
+++ assets/basicJavaScript/js/jsonDemo.js
@@ -0,0 +1,30 @@
+const jsonRequest = new Request("js/people.json");
+
+const people = [];
+
+class Person {
+  constructor(input) {
+    this.firstName = input.firstName;
+    this.lastName = input.lastName;
+    this.birthDate = new Date(input.birthDate);
+  }
+}
+
+function populatePeopleList(array) {
+  for (let i = 0; i < array.length; i++) {
+    const person = array[i];
+    let innerHTML = `<li>${person.firstName} ${person.lastName}</li>`;
+    document.getElementById("people").innerHTML += innerHTML;
+  }
+}
+
+fetch(jsonRequest)
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      const person = new Person(data[i]);
+      people.push(person);
+    }
+    populatePeopleList(people);
+  })
+  .catch(console.error);
